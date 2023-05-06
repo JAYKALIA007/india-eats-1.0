81f351ad74a4fb22ecbecc004ebbd92ceb3697f9
JAYKALIA007 <jaykalia047@gmail.com>
2023-05-06 18:58:29 +0530

    added animation to shimmer-(animate-pulse)

--- src/components/shimmer/ShimmerCardMenuPage.js
+++ src/components/shimmer/ShimmerCardMenuPage.js
@@ -3,9 +3,9 @@ import React from 'react'
 const ShimmerCardMenuPage = () => {
   return (
     <div className=' w-[600px] p-4 m-[60px] shadow-sm rounded-md ' >
-        <div className='w-full h-80  bg-slate-100 rounded-md ' ></div>
-        <p className='mt-4 h-4 bg-slate-100 w-1/2' ></p>
-        <p className='my-4 h-4 bg-slate-100 w-1/3 ' ></p>
+        <div className='w-full h-80  bg-slate-100 rounded-md  animate-pulse' ></div>
+        <p className='mt-4 h-4 bg-slate-100 w-1/2 animate-pulse' ></p>
+        <p className='my-4 h-4 bg-slate-100 w-1/3  animate-pulse' ></p>
     </div>
   )
 }
--- src/components/shimmer/ShimmerMenuPage.js
+++ src/components/shimmer/ShimmerMenuPage.js
@@ -3,8 +3,8 @@ import ShimmerCardMenuPage from './ShimmerCardMenuPage'
 const ShimmerMenuPage = () => {
   return (
     <div className=' mx-24 flex flex-wrap' >
-        <div className='mx-8 mt-10 my-2 h-4 bg-slate-100 w-full ' ></div>
-        <div className='mx-8 my-2 h-4 bg-slate-100 w-full ' ></div>
+        <div className='mx-8 mt-10 my-2 h-4 bg-slate-100 w-full animate-pulse ' ></div>
+        <div className='mx-8 my-2 h-4 bg-slate-100 w-full animate-pulse ' ></div>
         <ShimmerCardMenuPage />
         <ShimmerCardMenuPage />
     </div>
