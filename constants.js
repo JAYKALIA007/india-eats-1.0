9523e63e1505b40ab756663503d7ed02f78e44b9
Jay Prakash Kalia <jaykalia047@gmail.com>
2023-11-26 13:52:54 +0530

    fix url cors issues

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
-export const RESTAURANT_DATA_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9778905&lng=77.7371813&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
+export const RESTAURANT_DATA_URL = `https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9778905&lng=77.7371813&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
 export const IMAGE_CDN_URL = `https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/`
-// export const FETCH_MORE_RESTAURANT_DATA_URL = `https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`
+export const FETCH_MORE_RESTAURANT_DATA_URL = `https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`
 export const SEARCH_POPULAR_CUISINES_SUGGESTIONS = `https://corsproxy.io/?https://www.swiggy.com/dapi/landing/PRE_SEARCH?lat=12.9715987&lng=77.5945627`
-// export const SEARCH_SUGGESTIONS = `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=12.9715987&lng=77.5945627&str=me&trackingId=undefined`
+export const SEARCH_SUGGESTIONS = `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=12.9715987&lng=77.5945627&str=me&trackingId=undefined`
 
 export const SEARCH_SUGGESTIONS_IMAGE_CDN_URL = `https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/`
 export const RESTAURANT_MENU_URL = `https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=`
